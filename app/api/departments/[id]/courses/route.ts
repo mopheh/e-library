92c64c9164fe6995b08221084c09a1799d4b2388
Mopheh <omomofeadejare257@gmail.com>
2025-08-04 09:39:13 +0100

    route update

--- app/api/departments/[id]/courses/route.ts
+++ app/api/departments/[id]/courses/route.ts
@@ -1,17 +1,15 @@
-import { getCoursesByDepartment } from "@/actions/course";
-import { NextResponse } from "next/server";
+import { getCoursesByDepartment } from "@/actions/course"
+import { NextResponse } from "next/server"
 
-export async function GET(
-  req: Request,
-  { params }: { params: { id: string } },
-) {
+export async function GET(req: Request, context: { params: { id: string } }) {
+  const { id } = context.params
   try {
-    const courses = await getCoursesByDepartment(params.id);
-    return NextResponse.json(courses);
+    const courses = await getCoursesByDepartment(id)
+    return NextResponse.json(courses)
   } catch (error) {
     return NextResponse.json(
       { error: "Failed to fetch courses" },
-      { status: 500 },
-    );
+      { status: 500 }
+    )
   }
 }

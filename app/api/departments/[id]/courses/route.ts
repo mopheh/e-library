2ad6d84320032c0166f37fe1582aa7dee9de0236
Mopheh <omomofeadejare257@gmail.com>
2025-08-04 10:30:25 +0100

    clear dept/course

--- app/api/departments/[id]/courses/route.ts
+++ app/api/departments/[id]/courses/route.ts
@@ -1,15 +1,13 @@
 // import { getCoursesByDepartment } from "@/actions/course"
-// import { NextResponse } from "next/server"
+import { NextResponse } from "next/server"
 
-// export async function GET(req: Request, context: { params: { id: string } }) {
-//   const { id } = context.params
-//   try {
-//     const courses = await getCoursesByDepartment(id)
-//     return NextResponse.json(courses)
-//   } catch (error) {
-//     return NextResponse.json(
-//       { error: "Failed to fetch courses" },
-//       { status: 500 }
-//     )
-//   }
-// }
+export async function GET(req: Request) {
+  try {
+    return NextResponse.json({ message: "success" })
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Failed to fetch courses" },
+      { status: 500 }
+    )
+  }
+}

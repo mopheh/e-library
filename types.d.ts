d7a0d230df235909c2944b1e0920d631992395d6
Adejare <omomofeadejare257@gmail.com>
2025-10-12 01:42:53 +0100

    book row updated

--- types.d.ts
+++ types.d.ts
@@ -1,35 +1,36 @@
 interface Credentials {
-  id?: string
-  clerkId: string
-  fullName: string
-  email: string | undefined
-  year: string
-  role: string
-  facultyId: string
-  departmentId: string
-  matricNo: string
-  onboarded: boolean
-  gender: string // 👈 missing in your `Credentials`
-  address: string
-  createdAt?: string
-  lastActivityDate?: string
+  id?: string;
+  clerkId: string;
+  fullName: string;
+  email: string | undefined;
+  year: string;
+  role: string;
+  facultyId: string;
+  departmentId: string;
+  matricNo: string;
+  onboarded: boolean;
+  gender: string; // 👈 missing in your `Credentials`
+  address: string;
+  createdAt?: string;
+  lastActivityDate?: string;
 }
 type Faculty = {
-  id: string
-  name: string
-}
+  id: string;
+  name: string;
+};
 type Department = {
-  departmentName?: string
-  facultyId?: string
-  facultyName?: string
-  id?: string
-  name?: string
-}
+  departmentName?: string;
+  facultyId?: string;
+  facultyName?: string;
+  id?: string;
+  name?: string;
+};
 
 type Book = {
-  id: string
-  fileUrl: string | Null
-  title: string
-  type: string
-  createdAt: string
-}
+  id: string;
+  course: string;
+  fileUrl: string | Null;
+  title: string;
+  type: string;
+  createdAt: string;
+};
